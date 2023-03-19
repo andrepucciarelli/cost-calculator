fc0cfc5b7b4f12d935af80741d274d3b4ba32176
andrepucciarelli <andre.pucciarelli22@gmail.com>
2023-03-19 18:23:44 -0300

    continuacao da sidebar

--- components/sidebar.py
+++ components/sidebar.py
@@ -36,23 +36,29 @@ layout = dbc.Container([
     dbc.Row([
         dbc.Col([
             dbc.Nav([
-                dbc.NavItem(dbc.NavLink([html.I(className='fa fa-home dbc'), '\t INÍCIO'], href = '/home', active=True, style = {'text_align': 'center'})),
-                html.Br(),
-                dbc.NavItem(dbc.NavLink([html.I(className='fa-brands fa-font-awesome'), '\t NOVO MATERIAL'], href = '/home', active=True, style = {'text_align': 'center'})),
-                html.Br(),
-                dbc.NavItem(dbc.NavLink([html.I(className='fa fa-home dbc'), '\t NOVO PRODUTO'], href = '/home', active=True, style = {'text_align': 'center'})),
-                html.Br(),
-                dbc.NavItem(dbc.NavLink([html.I(className='fa fa-home dbc'), '\t NOVO ORÇAMENTO'], href = '/home', active=True, style = {'text_align': 'center'})),
-                html.Br()
+                dbc.NavItem(dbc.NavLink([html.I(className='fa fa-home dbc'), '\t INÍCIO'], href = '/home', active=True, style = {'text_align': 'left', 'font-size': '25px', 'margin-bottom': '10px'})),
+                dbc.NavItem(dbc.NavLink([html.I(className='fa fa-cube'), '\t NOVO MATERIAL'], id = 'botao_material', active=True, style = {'text_align': 'left', 'font-size': '25px', 'margin-bottom': '10px'})),
+                dbc.NavItem(dbc.NavLink([html.I(className='fa fa-cubes'), '\t NOVO PRODUTO'], id = 'botao_produto', active=True, style = {'text_align': 'left', 'font-size': '25px', 'margin-bottom': '10px'})),
+                dbc.NavItem(dbc.NavLink([html.I(className='fa fa-plus-square'), '\t NOVO ORÇAMENTO'], id = 'botao_orcamento', active=True, style = {'text_align': 'left', 'font-size': '25px', 'margin-bottom': '10px'})),
             ])
         ])
     ])
 
-], style = {'padding-top': '50px', 'margin-bottom': '100px'}, className='text-center')
+], style = {'padding-top': '50px', 'margin-bottom': '100px', 'height': '90vh'}, className='text-center')
 
 
 
 
 
 # =========  Callbacks  =========== #
-
+@app.callback(
+    Output('modal_novo_material', 'is_open'),
+    Input('modal_botao_salvar_material', 'n_clicks'),
+    Input('modal_botao_cancelar_material', 'n_clicks'),
+    Input('botao_material', 'n_clicks'),
+    State('modal_novo_material', 'is_open')
+)
+def modal_novo_material(n,n1,n2,is_open):
+    if n or n1 or n2:
+        return not is_open
+    return is_open
